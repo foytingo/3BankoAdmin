653061167ad47e902af2d43314b9f212f1a64e5e
Murat Baykor <foytingo@gmail.com>
2020-11-02 12:47:27 +0300

    Initial Commit

---
+++ 3Banko Admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3Banko Admin
+//
+//  Created by Murat Baykor on 2.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
